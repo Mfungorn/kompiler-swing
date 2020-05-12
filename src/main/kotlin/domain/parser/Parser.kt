2280cc9ab60c0fe37c117bfaed894ee0b76707d5
Michael <awayfromhead@gmail.com>
2020-05-12 18:28:30 +0700

    Added some errors neutralization in Parser (such as missing THEN or ELSE)

--- src/main/kotlin/domain/parser/Parser.kt
+++ src/main/kotlin/domain/parser/Parser.kt
@@ -7,59 +7,52 @@ class Parser {
         tokens: List<Token>,
         errors: List<String>
     ): ParserAnalysisResult {
-        val result = z(tokens, errors)
+        val hasElseBranch = tokens.contains(Else)
+
+        val result = tokens
+            .toResult(errors = errors)
+            .z(hasElseBranch)
         return ParserAnalysisResult(result.tokens, result.errors)
     }
 
-    private fun z(input: List<Token>, errors: List<String>): Result {
-        val hasElseBranch = input.contains(Else)
-        val result = input.toResult(errors = errors)
+    private fun Result.z(hasElseBranch: Boolean): Result = if (hasElseBranch)
+        `if`().b().then().a().`else`().a().endIf()
+    else
+        `if`().b().then().a().endIf()
 
-        return if (hasElseBranch)
-            result.`if`().b().then().a().`else`().a().endIf()
-        else
-            result.`if`().b().then().a().endIf()
+    private fun Result.`if`(): Result = if (currentToken is If) {
+        restTokens.toResult(listOf(currentToken), errors)
+    } else {
+        restTokens.toResult(listOf(If), errors + "Missing IF")
     }
 
-    private fun Result.`if`(): Result = if (currentToken is If)
-        restTokens.toResult(listOf(currentToken), errors)
-    else
-        restTokens.toResult(listOf(If), errors + "Missing IF")
-
-    private fun Result.then(): Result = if (currentToken is Then)
+    private fun Result.then(): Result = if (currentToken is Then) {
         restTokens.toResult(tokens + currentToken, errors)
-    else
-        restTokens.toResult(tokens + Then, errors + "Missing THEN")
+    } else { // Neutralization
+        this.copy(
+            tokens = tokens + Then,
+            errors = errors + "Missing THEN"
+        )
+    }
 
-    private fun Result.`else`(): Result = if (currentToken is Else)
+    private fun Result.`else`(): Result = if (currentToken is Else) {
         restTokens.toResult(tokens + currentToken, errors)
-    else
+    } else {
         restTokens.toResult(tokens + currentToken, errors + "Missing ELSE")
+    }
 
     private fun Result.endIf(): Result = if (currentToken is EndIf) {
-        Result(
-            currentToken,
-            emptyList(),
-            tokens + currentToken,
-            errors
-        )
+        Result(currentToken, emptyList(), tokens + currentToken, errors)
     } else {
-        Result(
-            currentToken,
-            emptyList(),
-            tokens + EndIf,
-            errors + "Missing END IF"
-        )
+        Result(currentToken, emptyList(), tokens + EndIf, errors + "Missing END IF")
     }
 
     // B -> CbB | C
-    private fun Result.b(): Result {
+    private fun Result.b(): Result { // TODO check if statement errors handles correctly
         val cResult = c()
-        val next = cResult.currentToken
 
-        return if (next in BOOLEAN_OPERATORS) {
-            cResult.restTokens
-                .toResult(cResult.tokens + next, errors)
+        return when (val next = cResult.currentToken) {
+            in BOOLEAN_OPERATORS -> cResult.restTokens.toResult(cResult.tokens + next, errors)
                 .b()
                 .let {
                     it.copy(
@@ -67,60 +60,77 @@ class Parser {
                         errors = errors + it.errors
                     )
                 }
-        } else {
-            // B -> C
-            cResult.copy(
-                tokens = tokens + cResult.tokens,
-                errors = errors + cResult.errors
-            )
+            is Then -> // B -> C
+                cResult.copy(
+                    tokens = tokens + cResult.tokens,
+                    errors = errors + cResult.errors
+                )
+            else -> { // Neutralization
+                cResult.copy(
+                    tokens = tokens + cResult.tokens,
+                    errors = errors + cResult.errors
+                )
+            }
         }
     }
 
     // C -> AlA
     private fun Result.c(): Result {
-        val firstAResult = a()
+        val firstAResult = a(isComparisonOperand = true)
         val next = firstAResult.currentToken
 
         return if (next in COMPARISON_OPERATORS) {
-            firstAResult.restTokens
-                .toResult(errors = errors)
-                .a()
+            firstAResult.restTokens.toResult(errors = errors)
+                .a(isComparisonOperand = true)
                 .let {
                     it.copy(
                         tokens = firstAResult.tokens + next + it.tokens,
                         errors = firstAResult.errors + it.errors
                     )
                 }
-        } else {
-            // error
-            TODO()
+        } else { // Neutralization
+            firstAResult.restTokens.toResult(
+                firstAResult.tokens + Operator.Equals + Operand("0"),
+                errors + "Missing condition"
+            )
         }
     }
 
-    private fun Result.a(): Result = when (val next = restTokens.first()) {
-        is Operand -> {
-            TODO() // error
-        }
-        in ARITHMETIC_OPERATORS -> {
-            restTokens
-                .takeLast(restTokens.size - 1)
-                .toResult(errors = errors)
-                .a()
-                .let {
-                    it.copy(
-                        tokens = listOf(currentToken) + next + it.tokens,
-                        errors = errors + it.errors
-                    )
+    // A -> AoA | i
+    private fun Result.a(isComparisonOperand: Boolean = false): Result = if (restTokens.isNotEmpty()) {
+        when (val next = restTokens.first()) {
+            in ARITHMETIC_OPERATORS -> {
+                restTokens
+                    .takeLast(restTokens.size - 1)
+                    .toResult(errors = errors)
+                    .a()
+                    .let {
+                        it.copy(
+                            tokens = listOf(currentToken) + next + it.tokens,
+                            errors = errors + it.errors
+                        )
+                    }
+            }
+            in COMPARISON_OPERATORS, in BOOLEAN_OPERATORS -> restTokens.toResult(listOf(currentToken), errors)
+            is Operand -> { // Neutralization
+                if (isComparisonOperand) {
+                    restTokens.toResult(tokens + currentToken, errors)
+                } else {
+                    restTokens
+                        .takeLast(restTokens.size - 1)
+                        .toResult(tokens + currentToken + Else + next, errors + "Missing ELSE")
                 }
+            }
+            else -> restTokens.toResult(tokens + currentToken, errors)
         }
-        in COMPARISON_OPERATORS, in BOOLEAN_OPERATORS -> restTokens.toResult(listOf(currentToken), errors)
-        else -> restTokens.toResult(tokens + currentToken, errors)
+    } else { // Neutralization
+        Result(EndIf, emptyList(), tokens + currentToken, errors + "Missing END IF")
     }
 
     private fun List<Token>.toResult(
         tokens: List<Token> = emptyList(),
         errors: List<String> = emptyList()
-    ) = Result(first(), takeLast(size - 1), tokens, errors)
+    ) = Result(first(), takeLast(size - 1), tokens, errors) // TODO size may be equals to 1
 
     data class ParserAnalysisResult(
         val tokens: List<Token>,
